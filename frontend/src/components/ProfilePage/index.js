908d5ac9e6c2da617a9b5800584556d89c2e9141
Richard Huang <rzhuangsg115@gmail.com>
2021-06-18 16:25:21 -0500

    redirect user out of profile page when log out

--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -1,4 +1,11 @@
+import { useSelector } from 'react-redux';
+import { Redirect } from "react-router-dom";
+
 const ProfilePage = () => {
+    const sessionUser = useSelector(state => state.session.user);
+
+    if (!sessionUser) return <Redirect to="/"/>;
+
     return (
         <div>
             <h1>HI</h1>
