50dcb9bdaeaa37db3f4d49a0bf38d0c809b4804a
Richard Huang <rzhuangsg115@gmail.com>
2021-06-18 12:29:19 -0500

    render links to login and sign up

---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from "react";
+
+
+const ProfileButton = () => {
+    return (
+        <>
+            <button >
+                <i className="fas fa-user"></i>
+            </button>
+        </>
+    );
+}
+
+export default ProfileButton;
---
+++ frontend/src/components/Navigation/index.js
@@ -0,0 +1,36 @@
+import ProfileButton from "./ProfileButton";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import {NavLink} from "react-router-dom";
+
+const Navigation = ({isLoaded}) => {
+    const sessionUser = useSelector(state => state.session.user);
+
+    let sessionLinks;
+    if (sessionUser) {
+        sessionLinks = (
+            <ProfileButton user={sessionUser}/>
+        );
+    }
+    else {
+        sessionLinks = (
+            <>
+                <NavLink to="/login">Log In</NavLink>
+                <NavLink to="/signup">Sign Up</NavLink>
+            </>
+        );
+    }
+
+    return (
+        <>
+            <ul>
+                <li>
+                    <NavLink exact to="/">Home</NavLink>
+                    {isLoaded && sessionLinks}
+                </li>
+            </ul>
+        </>
+    );
+}
+
+export default Navigation;
