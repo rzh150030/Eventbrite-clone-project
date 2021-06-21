d06ebd1d75e7c2f2d25d21f21d95eb9605780dfd
Richard Huang <rzhuangsg115@gmail.com>
2021-06-21 09:18:45 -0500

    create seeds for job titles

--- backend/db/seeders/20210621140051-job-titles.js
+++ backend/db/seeders/20210621140051-job-titles.js
@@ -2,16 +2,57 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+      return queryInterface.bulkInsert('Job_titles', [
+        {title: "Cloud Architect"},
+        {title: "Cloud Consultant"},
+        {title: "Cloud Services Developer"},
+        {title: "Cloud System Engineer"},
+        {title: "Cloud System Administrator"},
+        {title: "Cloud System Engineer"},
+        {title: "Cloud Product and Project Manager"},
+        {title: "Computer Network Architect"},
+        {title: "Computer Systems Analyst"},
+        {title: "Computer Systems Manager"},
+        {title: "IT Analyst"},
+        {title: "IT Coordinator"},
+        {title: "Network Administrator"},
+        {title: "Network Architect"},
+        {title: "Network and Computer Systems Administrator"},
+        {title: "Network Engineer"},
+        {title: "Network Systems Administrator"},
+        {title: "Senior Network Engineer"},
+        {title: "Senior Network Architect"},
+        {title: "Senior Network System Administrator"},
+        {title: "Telecommunications Specialist"},
+        {title: "Data Center Support Specialist"},
+        {title: "Data Quality Manager"},
+        {title: "Application Support Analyst"},
+        {title: "Senior System Analyst"},
+        {title: "Systems Analyst"},
+        {title: "Systems Designer"},
+        {title: "Information Security Analyst"},
+        {title: "Information Security Analyst"},
+        {title: "Security Specialist"},
+        {title: "Senior Security Specialist"},
+        {title: "Application Developer"},
+        {title: "Associate Developer"},
+        {title: "Junior Software Engineer"},
+        {title: "Programmer Analyst"},
+        {title: "Senior Programmer Analyst"},
+        {title: "Senior Software Engineer"},
+        {title: "Senior System Architect"},
+        {title: "Senior System Designer"},
+        {title: "Senior Systems Software Engineer"},
+        {title: "Software Architect"},
+        {title: "Software Engineer"},
+        {title: "Software Quality Assurance Analyst"},
+        {title: "System Architect"},
+        {title: "Systems Software Engineer"},
+        {title: "Front End Developer"},
+        {title: "Senior Web Administrator"},
+        {title: "Web Developer"}
+      ], {});
 
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
   },
 
   down: (queryInterface, Sequelize) => {
