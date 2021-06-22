8682250d53255c9d630091abcadb150fb279fe8a
Richard Huang <rzhuangsg115@gmail.com>
2021-06-22 13:08:43 -0500

    changing to datePicker

--- frontend/src/components/CreateFairPage/index.js
+++ frontend/src/components/CreateFairPage/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { postEventFair } from '../../store/careerfair';
 import Calendar from 'react-calendar';
+import DatePicker from "react-datepicker";
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from "react-router-dom";
 import "./CreateFairPage.css";
@@ -12,7 +13,8 @@ export default function CreateFairPage() { //should only be accessible to logged
     const [venue_id, setVenueId] = useState("");
     const [name, setName] = useState("");
     const [date, setDate] = useState(new Date());
-    const [capacity, setCapacity] = useState(0);
+    const [time, setTime] = useState("9:00 PM");
+    const [capacity, setCapacity] = useState("");
 
     if (!sessionUser) {
         history.push("/login")
@@ -25,6 +27,8 @@ export default function CreateFairPage() { //should only be accessible to logged
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        //attach time to the date
 
         const event = {
             host_id: sessionUser.id, //sessionUser.id to get current logged in user id
@@ -44,7 +48,9 @@ export default function CreateFairPage() { //should only be accessible to logged
         <div>
             <form>
                 <input type="text" placeholder="Event Name" value={name} onChange={addName} required />
-                <Calendar value={date} onChange={setDate}/>
+                {/* <Calendar value={date} onChange={setDate}/> */}
+                <DatePicker selected={date} onSelect={setDate} showTimeSelect dateFormat="MMMM d, yyyy h:mm aa"/>
+                {/* <input type="time" value={time} onChange={setTime} required /> */}
                 <input type="text" placeholder="Capacity" value={capacity} onChange={addCapacity} required />
             </form>
         </div>
