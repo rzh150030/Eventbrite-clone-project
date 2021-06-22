a603492ad9ae2074c3255ae81b76a17e796a0a0c
Richard Huang <rzhuangsg115@gmail.com>
2021-06-22 11:41:28 -0500

    use react-calendar to set date, troubleshooting its use

--- frontend/src/components/CreateFairPage/index.js
+++ frontend/src/components/CreateFairPage/index.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { postEventFair } from '../../store/careerfair';
+import Calendar from 'react-calendar';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from "react-router-dom";
 import "./CreateFairPage.css";
 
 export default function CreateFairPage() { //should only be accessible to logged in users
     let history = useHistory();
+    const dispatch = useDispatch();
     const sessionUser = useSelector(state => state.session.user);
+    const [name, setName] = useState("");
+    const [venue_id, setVenueId] = useState("");
+    const [date, setDate] = useState(new Date());
 
     if (!sessionUser) {
         history.push("/login")
     }
-    else{
-        console.log(sessionUser.id)
-    }
+    //sessionUser.id to get current logged in user id
+    //onChange event handlers
+    const addName = (e) => setName(e.target.value);
+    const addVenue = (e) => setVenueId(e.target.value);
+    const addDate = (e) => setDate(e.target.value);
 
-    const handleSubmit = () => {
-
+    const handleSubmit = (e) => {
+        e.preventDefault()
     }
 
     return (
@@ -24,6 +31,7 @@ export default function CreateFairPage() { //should only be accessible to logged
             <form>
                 <input type="text" placeholder="Event Name" required>
                 </input>
+                <Calendar value={date} onChange={setDate}/>
             </form>
         </div>
     )
