7b63f3c65701185af584cad8fab55345ca4d2bb6
Richard Huang <rzhuangsg115@gmail.com>
2021-06-22 12:08:18 -0500

    working on calendar to set date

--- frontend/src/components/CreateFairPage/index.js
+++ frontend/src/components/CreateFairPage/index.js
@@ -9,21 +9,35 @@ export default function CreateFairPage() { //should only be accessible to logged
     let history = useHistory();
     const dispatch = useDispatch();
     const sessionUser = useSelector(state => state.session.user);
-    const [name, setName] = useState("");
     const [venue_id, setVenueId] = useState("");
+    const [name, setName] = useState("");
     const [date, setDate] = useState(new Date());
+    const [capacity, setCapacity] = useState(0);
 
     if (!sessionUser) {
         history.push("/login")
     }
-    //sessionUser.id to get current logged in user id
+
     //onChange event handlers
     const addName = (e) => setName(e.target.value);
     const addVenue = (e) => setVenueId(e.target.value);
-    const addDate = (e) => setDate(e.target.value);
+    const addDate = (e) => setDate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const event = {
+            host_id: sessionUser.id, //sessionUser.id to get current logged in user id
+            venue_id,
+            name,
+            date,
+            capacity
+        }
+
+        let newEvent = await dispatch(postEventFair(event))
+        if (newEvent) {
+            history.push(`/event/${newEvent.id}`);
+        }
     }
 
     return (
@@ -31,7 +45,7 @@ export default function CreateFairPage() { //should only be accessible to logged
             <form>
                 <input type="text" placeholder="Event Name" required>
                 </input>
-                <Calendar value={date} onChange={setDate}/>
+                <Calendar value={date} onChange={addDate}/>
             </form>
         </div>
     )
