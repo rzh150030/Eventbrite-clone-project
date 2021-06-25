078e01fff17ac9074b212ea7899fb9d46214a2df
Richard Huang <rzhuangsg115@gmail.com>
2021-06-25 11:48:14 -0500

    create notes for which thunks to make

--- frontend/src/store/registration.js
+++ frontend/src/store/registration.js
@@ -4,6 +4,12 @@ const REGISTER_EVENT = "registerFair/registerEvent";
 const LOAD_REGISTERS = "registerFair/registeredEvents";
 const UNREGISTER_EVENT = "registerFair/UnregisterEvent";
 
+//thunk for registering to event
+
+//thunk for getting user's registered events
+
+//thunk for unregistering from event
+
 const initialState = {registrations: {}};
 
 const registerReducer = (state = initialState, action) => {
