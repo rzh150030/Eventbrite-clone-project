fc005cf74b7ef0f57fc9cb374e20c7a12b64f22b
Richard Huang <rzhuangsg115@gmail.com>
2021-06-25 11:21:54 -0500

    made the necessary routes for registrations

--- backend/routes/api/registration.js
+++ backend/routes/api/registration.js
@@ -1,17 +1,40 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 const {requireAuth} = require("../../utils/auth");
-const {Registration} = require("../../db/models");
+const {Registration, Career_fair, User} = require("../../db/models");
 
 const router = express.Router();
 
 //Create a new registration
 router.post("/register", requireAuth, asyncHandler(async (req, res) => {
-    
+    const {career_fair_id, user_id} = req.body;
+
+    const register = await Registration.create({
+        career_fair_id,
+        user_id
+    });
+
+    const registration = await Registration.findByPk(register.id, {include: [Career_fair, User]});
+
+    res.json(registration);
 }));
 
 //Delete an existing registration
+router.delete("/:id(\\d+)/unregister", requireAuth, asyncHandler(async (req, res) => {
+    const id = req.params.id;
 
-//Get all registrations
+    const registration = await Registration.findByPk(id);
+    await registration.destroy();
+    return res.json("Unregistered");
+}));
+
+//Get all user's registrations
+router.get("/:id(\\d+)/registrations", requireAuth, asyncHandler(async (req, res) => {
+    const id = req.params.id;
+
+    const user = await User.findByPk(id, {include: Registration});
+
+    return res.json(user);
+}));
 
 module.exports = router;
