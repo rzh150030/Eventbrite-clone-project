27a8a312880d17ba1d88058aeed201549d8e985d
Richard Huang <rzhuangsg115@gmail.com>
2021-06-25 11:46:26 -0500

    create type for loading user registrations

--- frontend/src/store/registration.js
+++ frontend/src/store/registration.js
@@ -1,6 +1,7 @@
 import { csrfFetch } from './csrf';
 
 const REGISTER_EVENT = "registerFair/registerEvent";
+const LOAD_REGISTERS = "registerFair/registeredEvents";
 
 const initialState = {registrations: {}};
 
