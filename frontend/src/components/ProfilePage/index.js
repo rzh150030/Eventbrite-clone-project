e8fabe01dce70226464ef22a1f8dab0695268629
Richard Huang <rzhuangsg115@gmail.com>
2021-06-26 16:08:17 -0500

    made registered events appear on the right side of the page

--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -30,23 +30,27 @@ const ProfilePage = () => {
                 <span>Events Hosted</span>
                 <span>Registered Events</span>
             </div>
-            <div className="hosted-event-container">
-                {userEvents && userEvents.map(event => (
-                    <div className="host-event-container" key={event.id}>
-                        <NavLink to={`/event/${event.id}`} className="hosted-event-links">
-                            {event.name}
-                        </NavLink>
-                        <span>{convertDate(event.date)}</span>
-                    </div>
-                ))}
-                {userRegistrations && userRegistrations.map(register => (
-                    <div className="registration-container" key={register.id}>
-                        <NavLink to={`/event/${register.career_fair_id}`}>
-                            {register.Career_fair.name}
-                        </NavLink>
-                        <span>{convertDate(register.Career_fair.date)}</span>
-                    </div>
-                ))}
+            <div className="profile-items">
+                <div className="hosted-events-container">
+                    {userEvents && userEvents.map(event => (
+                        <div className="host-event-container" key={event.id}>
+                            <NavLink to={`/event/${event.id}`} className="hosted-event-links">
+                                {event.name}
+                            </NavLink>
+                            <span>{convertDate(event.date)}</span>
+                        </div>
+                    ))}
+                </div>
+                <div className="user-registrations-container">
+                    {userRegistrations && userRegistrations.map(register => (
+                        <div className="registration-container" key={register.id}>
+                            <NavLink to={`/event/${register.career_fair_id}`}>
+                                {register.Career_fair.name}
+                            </NavLink>
+                            <span>{convertDate(register.Career_fair.date)}</span>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
