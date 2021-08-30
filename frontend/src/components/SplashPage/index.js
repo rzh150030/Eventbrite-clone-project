2d1969bc5a475bd1e210abced76fdd32f287910f
Richard Huang <rzhuangsg115@gmail.com>
2021-08-30 16:48:23 -0500

    contain splash page in a div and add css file

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -4,7 +4,10 @@ import splashImage from "../../images/cascade-job-fair-1.jpg";
 export default function SplashPage() {
     return (
         <div className="splash-page">
-            <img src={splashImage} alt="job fair"/>
+            <div className="splash-page-image-layer">
+                <img id="splash-image" src={splashImage} alt="job fair"/>
+                
+            </div>
         </div>
     );
 }
