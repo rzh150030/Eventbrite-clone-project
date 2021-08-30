0163dcbea9bd23cca21194e0c3a2fb5aba0529cd
Richard Huang <rzhuangsg115@gmail.com>
2021-08-30 17:43:28 -0500

    add button and splash message

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -6,8 +6,11 @@ export default function SplashPage() {
         <div className="splash-page">
             <div className="splash-page-image-layer">
                 <img id="splash-image" src={splashImage} alt="job fair"/>
-                
+                <div id="splash-message-container">
+                    <p>Go get a job!</p>
+                </div>
             </div>
+            <button>Find a job fair</button>
         </div>
     );
 }
