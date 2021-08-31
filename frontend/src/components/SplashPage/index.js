ee65ccc85f5283636847095eeeb0147d1cf717d4
Richard Huang <rzhuangsg115@gmail.com>
2021-08-30 20:07:38 -0500

    create usestate for 3 most recent events and invoke in useffect

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import splashImage from "../../images/cascade-job-fair-1.jpg";
 
-
 export default function SplashPage() {
+    const eventsList = useSelector(state => Object.values(state.careerFair.event));
+    const [recentEvents, setRecentEvents] = useState([]);
+
+    useEffect(() => {   //grab 3 most recent events
+        const list = [];
+        for (let i = 0; i < 3; i++) {
+            list.push(eventsList[i]);
+        }
+        setRecentEvents(list);
+    }, [eventsList, recentEvents]);
+
+
     return (
         <div className="splash-page">
             <div className="splash-page-image-layer">
@@ -9,8 +22,12 @@ export default function SplashPage() {
                 <div id="splash-message-container">
                     <p>Go get a job!</p>
                 </div>
+                <button>Find a job fair</button>
             </div>
-            <button>Find a job fair</button>
+            <span>Most recent events</span>
+            <div className="splash-recent-events">
+
+            </div>
         </div>
     );
 }
