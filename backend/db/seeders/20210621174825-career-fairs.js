e5125a83410388a0d09d6ef3ffaf036d7c061e0f
Richard Huang <rzhuangsg115@gmail.com>
2021-09-03 11:06:15 -0500

    add cascade delete on careerfair and a another event

--- backend/db/seeders/20210621174825-career-fairs.js
+++ backend/db/seeders/20210621174825-career-fairs.js
@@ -3,9 +3,10 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
       return queryInterface.bulkInsert('Career_fairs', [
-        {host_id: 3, venue_id: 2, name: "MacroHard Career Meetup", date: new Date("August 7, 2021 2:00 PM"), capacity: 100},
+        {host_id: 3, venue_id: 2, name: "MacroHard Career Meetup 2021", date: new Date("August 7, 2021 2:00 PM"), capacity: 100},
         {host_id: 4, venue_id: 1, name: "Moogle's Annual Job Fair 2021", date: new Date("December 7, 2021 11:00 AM"), capacity: 500},
-        {host_id: 2, venue_id: 5, name: "Software Engineering Event", date: new Date("January 20, 2022 1:00 PM"), capacity: 500}
+        {host_id: 2, venue_id: 5, name: "Software Engineering Event", date: new Date("January 20, 2022 1:00 PM"), capacity: 500},
+        {host_id: 3, venue_id: 2, name: "MacroHard Career Meetup 2022", date: new Date("August 7, 2022 2:00 PM"), capacity: 100}
       ], {});
 
   },
